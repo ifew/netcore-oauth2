96f53112ba9d332e3ec8323b8ae8dcd81fb96d7d
iFew <chitpong@gmail.com>
2018-11-30 11:41:10 +0700

    Added Implicit authen, and refactoring all for support test 2 type authenticate

---
+++ client/Implicit.js
@@ -0,0 +1,64 @@
+$( "#login" ).click(function() {
+    login();
+});
+$( "#api" ).click(function() {
+    api();
+});
+$( "#logout" ).click(function() {
+    logout();
+});
+
+var config = {
+    authority: "http://localhost:5001",
+    client_id: "clientJS",
+    redirect_uri: "http://localhost:5000/callback.html",
+    response_type: "id_token token",
+    scope: "profile User.info",
+    post_logout_redirect_uri : "http://localhost:5000/logout.html",
+};
+var mgr = new Oidc.UserManager(config);
+
+mgr.getUser().then(function (user) {
+    if (user) {
+        log("User logged in", user.profile);
+    }
+    else {
+        log("User not logged in");
+    }
+});
+
+function login() {
+    mgr.signinRedirect();
+}
+
+function api() {
+    mgr.getUser().then(function (user) {
+        var url = "http://localhost:5001/api/user";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function () {
+            log(xhr.status, JSON.parse(xhr.responseText));
+        }
+        xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
+        xhr.send();
+    });
+}
+
+function logout() {
+    mgr.signoutRedirect();
+}
+
+function log() {
+    document.getElementById('results').innerText = '';
+
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (msg instanceof Error) {
+            msg = "Error: " + msg.message;
+        }
+        else if (typeof msg !== 'string') {
+            msg = JSON.stringify(msg, null, 2);
+        }
+        document.getElementById('results').innerHTML += msg + '\r\n';
+    });
+}
